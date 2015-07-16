875f52ff8540759b88f7623d2639abe2ae8b1349
Adam Stokes <adam.stokes@ubuntu.com>
2015-07-16 10:38:45 -0400

    test true
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- test/index.js
+++ test/index.js
@@ -6,6 +6,12 @@ var isFileP = require("..");
 require("should");
 
 describe("node module", function() {
+    it("should send back true for files", function(done){
+        isFileP(__filename).then(function(bool){
+            bool.should.be.true;
+            done();
+        });
+    });
     it("should catch errors", function(done){
         isFileP("/aadfffdsafda").catch(function(err){
             err.should.be.an.instanceOf(Error);
