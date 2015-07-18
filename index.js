69746d55a2209c3fa2191997889baf37633f89ed
Adam Stokes <adam.stokes@ubuntu.com>
2015-07-17 23:51:47 -0400

    Add native-or-bluebird dep for easy promise migration
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 "use strict";
 
+var promise = require("native-or-bluebird");
 var isFile = require("is-file");
 
 module.exports = function(path){
-    return new Promise(function(_resolve, _reject){
+    return new promise(function(_resolve, _reject){
         isFile(path, function(err, stats){
             return err === null ? _resolve(stats) : _reject(err);
         });
