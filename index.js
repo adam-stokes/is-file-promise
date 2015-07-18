0690332103eac982b778088a143d8dc4d2e93837
Adam Stokes <adam.stokes@ubuntu.com>
2015-07-18 14:36:40 -0400

    fix eslint errors
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 "use strict";
 
-var promise = require("native-or-bluebird");
+var Promise = require("native-or-bluebird");
 var isFile = require("is-file");
 
 module.exports = function(path){
-    return new promise(function(_resolve, _reject){
+    return new Promise(function(_resolve, _reject){
         isFile(path, function(err, stats){
             return err === null ? _resolve(stats) : _reject(err);
         });
-    }).catch(function(err){
-        throw err;
     });
 };
