7ea4eeb1903aa1648b952b626c82867b472aa5c8
Adam Stokes <adam.stokes@ubuntu.com>
2016-02-01 10:21:49 -0500

    Use native promises
    Fixes: #1
    
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 "use strict";
 
-var Promise = require("native-or-bluebird");
 var isFile = require("is-file");
 
-module.exports = function(path){
-    return new Promise(function(_resolve, _reject){
-        isFile(path, function(err, stats){
-            return err === null ? _resolve(stats) : _reject(err);
-        });
+module.exports = function(path) {
+  return new Promise(function(_resolve, _reject) {
+    isFile(path, function(err, stats) {
+      return err === null ? _resolve(stats) : _reject(err);
     });
+  });
 };
--- test/index.js
+++ test/index.js
@@ -6,16 +6,16 @@ var isFileP = require("..");
 require("should");
 
 describe("node module", function() {
-    it("should send back true for files", function(done){
-        isFileP(__filename).then(function(bool){
-            bool.should.be.true;
-            done();
-        });
+  it("should send back true for files", function(done) {
+    isFileP(__filename).then(function(bool) {
+      bool.should.be.true;
+      done();
     });
-    it("should catch errors", function(done){
-        isFileP("/aadfffdsafda").catch(function(err){
-            err.should.be.an.instanceOf(Error);
-            done();
-        });
+  });
+  it("should catch errors", function(done) {
+    isFileP("/aadfffdsafda").catch(function(err) {
+      err.should.be.an.instanceOf(Error);
+      done();
     });
+  });
 });
